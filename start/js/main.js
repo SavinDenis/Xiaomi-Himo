42fe7e73e435592befe8a404752c81a1f5af9dd9
SavinDenis <116026979+SavinDenis@users.noreply.github.com>
2022-11-08 16:09:30 +0600

    create

---
+++ start/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    
+    
+    $('.bike_slider, .bike_slider_jpg').slick({
+        arrows: false,
+        dots:true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
